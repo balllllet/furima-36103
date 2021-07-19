85913332c5746570b8ff0fbbdaa9efe0550a24f2
balllllet <soyoka12345@icloud.com>
2021-07-19 15:27:07 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,9 @@ class ItemsController < ApplicationController
    
  end
 
- def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
+ #def move_to_index
+  #unless user_signed_in?
+  #  redirect_to action: :index
+  #end
 end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  
   with_options presence: true do
     validates :nickname       
-    validates :birthday         
-  end
+    validates :birthday  
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :family_name
-    validates :first_name      
-  end
+      with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+        validates :family_name
+        validates :first_name      
+      end
 
-  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/} do
-    validates :family_name_reading
-    validates :first_name_reading
-  end
+      with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/} do
+        validates :family_name_reading
+        validates :first_name_reading
+      end
+
+  end    
 
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  
-   
   has_many :items
   has_many :purchases
-end
+  end
+  

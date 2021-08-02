bd31d80a1e7e543b246e2b7adf20414e2e845cce
balllllet <soyoka12345@icloud.com>
2021-08-02 17:20:03 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
   before_action :find_id, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  before_action :edit_root, only: [:edit]
+  before_action :edit_root, only: [:edit, :update, :destroy]
 
 
  def index
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -59,6 +59,12 @@ RSpec.describe OrderAddress, type: :model do
       expect(@order.errors.full_messages).to include("Prefecture can't be blank")
     end
 
+    it "都道府県が---では登録できないこと" do
+      @order.prefecture_id = 0
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Prefecture must be other than 0")
+    end
+
     it "市区町村が空では登録できないこと" do
       @order.city = nil
       @order.valid?
